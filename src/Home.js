c3197699db7796a588f9ae992371cc8cc5f49b98
Swastik Yadav <swastikyadav.dev@gmail.com>
2019-07-26 19:16:15 +0530

    my first react app

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,31 @@ import React from 'react'
 
 export default function Home(){
     return(
-        <h1>hello</h1>
+        <div className="timeline">
+    <div className="container left">
+      <div className="content">
+        <h2>2017</h2>
+        <p>Lorem ipsum..</p>
+      </div>
+    </div>
+    <div className="container right">
+      <div className="content">
+        <h2>2016</h2>
+        <p>Lorem ipsum..</p>
+      </div>
+    </div>
+    <div className="container left">
+      <div className="content">
+        <h2>2016</h2>
+        <p>Lorem ipsum..</p>
+      </div>
+    </div>
+    <div className="container right">
+      <div className="content">
+        <h2>2016</h2>
+        <p>Lorem ipsum..</p>
+      </div>
+      </div>
+      </div>
     )
 }
