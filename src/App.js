87f87461bedd1b6d0f18c7801d15b97718f42040
akslucky619 <akash619@protonmail.com>
2019-07-26 19:08:49 +0530

    added home component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
      <Router>
+     <Route exact path="/" component={Home} />
        <Route exact path="/string" component={String} />
      </Router>
     </div>
